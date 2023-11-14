aabe10be78d621eef9fc017004f5f4a9f50c5ed0
alanbustamante733 <alanbustamante733@gmail.com>
2023-11-14 12:23:46 +0100

    Initialisierung

---
+++ POO/src/main/java/Logica/POO.java
@@ -0,0 +1,12 @@
+
+package Logica;
+
+public class POO {
+    
+    public static void main(String[] args) {
+        
+        Alumno alu1 = new Alumno ();
+        Alumno alu2 = new Alumno (35, "Luisina", "de Paula");
+    }
+    
+}
